9c95eaf0583e6bb5d5dd1b882c3dcc16585436bf
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-14 16:39:20 -0600

    Initial Commit

---
+++ Shared/StackGifApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackGifApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct StackGifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
