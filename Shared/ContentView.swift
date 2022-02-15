dbf13a306ba1c3fc6979ae6164900ae48d10e846
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-14 18:49:53 -0600

    More explain

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,6 +11,12 @@
 
 ///A stack gives you a LIFO or last-in first-out order. The element you pushed last is the first one to come off with the next pop. (A very similar data structure, the queue, is FIFO or first-in first-out.)
 //
+//
+//
+//Notice that a push puts the new element at the end of the array, not the beginning. Inserting at the beginning of an array is expensive, an O(n) operation, because it requires all existing array elements to be shifted in memory. Adding at the end is O(1); it always takes the same amount of time, regardless of the size of the array.
+
+///Fun fact about stacks: Each time you call a function or a method, the CPU places the return address on a stack. When the function ends, the CPU uses that return address to jump back to the caller. That's why if you call too many functions -- for example in a recursive function that never ends -- you get a so-called "stack overflow" as the CPU stack has run out of space.
+//
 import SwiftUI
 
 struct ContentView: View {
