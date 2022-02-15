881ad79e5817b12aa3cb597db1a3e7fc969661ac
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-14 18:44:11 -0600

    A stack explain
    A stack is like an array but with limited functionality

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,7 +4,13 @@
 //
 //  Created by Borna Libertines on 14/02/22.
 //
+//
+///A stack is like an array but with limited functionality. You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off.
 
+///Why would you want to do this? Well, in many algorithms you want to add objects to a temporary list at some point and then pull them off this list again at a later time. Often the order in which you add and remove these objects matters.
+
+///A stack gives you a LIFO or last-in first-out order. The element you pushed last is the first one to come off with the next pop. (A very similar data structure, the queue, is FIFO or first-in first-out.)
+//
 import SwiftUI
 
 struct ContentView: View {
